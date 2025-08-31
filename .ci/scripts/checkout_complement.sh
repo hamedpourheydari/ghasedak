7319687caf1fdb2dc10f896ffa7f6af6393034f4
hamed pourheydari <113162536+hamedpourheydari@users.noreply.github.com>
2025-08-31 10:59:35 +0330

    Update checkout_complement.sh

--- .ci/scripts/checkout_complement.sh
+++ .ci/scripts/checkout_complement.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #
-# Fetches a version of complement which best matches the current build.
+# نسخه‌ای از complement را دریافت می‌کند که بهترین تطابق را با ساخت فعلی دارد.
 #
 # The tarball is unpacked into `./complement`.
+#این اسکریپت شِل (bash) وظیفه دانلود و استخراج یک نسخه خاص از پروژه Complement (یک ابزار تست برای سرورهای Matrix): 
+ # را دارد، به گونه‌ای که با شاخه یا نسخه فعلی پروژه شما تطابق داشته باشد. در ادامه توضیح خط به خط و کارکرد کلی آن آمده است
+ #این اسکریپت یک نسخه مناسب از Complement را از گیت‌هاب دانلود می‌کند و آن را در پوشهٔ ./complement استخراج می‌کند. 
+ #انتخاب نسخه بر اساس شاخه فعلی گیت‌هاب (در محیط CI/CD مثل GitHub Actions) انجام می‌شود.
 
 set -e
-mkdir -p complement
+#    اگر هر دستوری در اسکریپت با خطا مواجه شود، اجرای اسکریپت متوقف می‌شود (fail-fast). 
 
-# Pick an appropriate version of complement. Depending on whether this is a PR or release,
-# etc. we need to use different fallbacks:
-#
-# 1. First check if there's a similarly named branch (GITHUB_HEAD_REF
-#    for pull requests, otherwise GITHUB_REF).
-# 2. Attempt to use the base branch, e.g. when merging into release-vX.Y
-#    (GITHUB_BASE_REF for pull requests).
-# 3. Use the default complement branch ("HEAD").
+mkdir -p complement
+#    پوشه complement را ایجاد می‌کند (اگر وجود نداشته باشد). 
+     
+
 for BRANCH_NAME in "$GITHUB_HEAD_REF" "$GITHUB_BASE_REF" "${GITHUB_REF#refs/heads/}" "HEAD"; do
   # Skip empty branch names and merge commits.
   if [[ -z "$BRANCH_NAME" || $BRANCH_NAME =~ ^refs/pull/.* ]]; then
